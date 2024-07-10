bc57e55619fc29be26056ec2a0af22deac098c90
Sresthasa@09 <153790855+Sresthasa2026@users.noreply.github.com>
2024-07-10 10:55:41 +0530

    Add files via upload

---
+++ Disney-Hotstar-Clone-main/Disney-Hotstar-Clone-main/JAVASCRIPT/function.js
@@ -0,0 +1,25 @@
+/*function greet(){
+console.log("hi")}
+greet()
+function sum(a,b)
+{
+    console.log("sum: ");
+    return a+b;
+}
+console.log(sum(5,6))
+function fact(num)
+{
+    if(num==0)
+       return 1
+    return num*fact(num-1)
+}
+console.log(fact(5))
+const hello=function hello_fun()
+{
+    console.log("hello")
+}
+hello();*/
+const s=function (a,b){
+    return a+b;
+}
+console.log(s(2,3))
